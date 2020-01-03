7dfb51e916e463a297b875d55e1493702d00d674
guruJosh <gutuJosh@users.noreply.github.com>
2020-01-03 13:56:54 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import FollowFaces from "./FollowFaces";
+import './style.css'
+import './image-component'
+
+const faces = new FollowFaces('.strip-holder');
---
+++ src/image-component.js
@@ -0,0 +1,10 @@
+import image from "./m_over.jpg"; 
+
+const docFrag = document.createDocumentFragment();
+const img = document.createElement("img");
+img.src = image;
+const stripHolder = document.createElement("div");
+stripHolder.className = "strip-holder";
+stripHolder.appendChild(img);
+docFrag.appendChild(stripHolder);
+document.body.appendChild(docFrag)
